0dcfed6cd2b53c2a7303e30bcf6779e604b85a63
Harsh Soni <hsoni7793@gmail.com>
2018-03-08 15:00:47 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-function 
+/*
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    $("#info").css('display',"block");
+    $("#loggedout").css("display" , "none");
+    $("#username").text(profile.getName());
+    $("#image").attr('src',profile.getImageUrl());
+    $("#email").text(profile.getEmail());
+  }
+  
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      $("#loggedout").css("display" , "block");
+      $(".g-signin2").css("display" , "block");
+      $("#info").css("display" , "none");
+    });
+*/
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); 
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  }
+}
